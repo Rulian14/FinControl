86b648ebdefb428fcd6a412cb1a02e8e57d690ea
GustavoChaicouski <gustav0.chaic0usk@gmail.com>
2026-04-16 15:05:02 -0300

    Estrutura inicial do FinControl)

---
+++ backend/fincontrol-api/src/main/java/com/fincontrol/FincontrolApiApplication.java
@@ -0,0 +1,13 @@
+package com.fincontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincontrolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincontrolApiApplication.class, args);
+	}
+
+}
---
+++ backend/fincontrol-api/src/test/java/com/fincontrol/TesteDependencias.java
@@ -0,0 +1,14 @@
+package com.fincontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import com.fincontrol.FincontrolApiApplication;
+
+class TesteDependencias {
+
+    @Test
+    void contextoCarrega() {
+
+        System.out.println("Spring subiu 🚀");
+    }
+}
